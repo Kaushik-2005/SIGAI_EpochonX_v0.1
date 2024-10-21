7b6d2fd730619fba279cb6a078bdc074c9e2de4f
K4u5hik <kaushik.kesanapalli@gmail.com>
2024-10-21 14:20:12 +0530

    first commit

---
+++ generator/docx.py
@@ -0,0 +1,9 @@
+# Generate a DOCX file
+from docx import Document
+
+class DocxGenerator:
+    def __init__(self):
+        pass
+    
+    def generate(self, prompt: str):
+        pass
---
+++ generator/pdf.py
@@ -0,0 +1,8 @@
+# Generate a PDF file
+
+class PDFGenerator:
+    def __init__(self):
+        pass
+    
+    def generate(self, prompt: str):
+        pass
